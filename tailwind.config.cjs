1be75a59d1c46cf6fa652a082fcf03dc0e199959
Sama-E <112719147+Sama-E@users.noreply.github.com>
2025-03-04 10:34:17 -0500

    clean slate

---
+++ tailwind.config.cjs
@@ -0,0 +1,40 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "gray-20": "#FFFFFF",
+        "gray-50": "#dfebf0",
+        "gray-100": "#ccdbdf",
+        "gray-500": "#00475e",
+        "primary-100": "#e0f7ff",
+        "primary-300": "#a3e2ff",
+        "primary-500": "#66bfff",
+        "secondary-400": "#9c641f",
+        "secondary-500": "#ffc85b",
+      },
+      backgroundImage: (theme) => ({
+        "gradient-yellowred":
+          "linear-gradient(90deg, #FF616A 0%, #FFC837 100%)",
+      }),
+      fontFamily: {
+        dmsans: ["DM Sans", "sans-serif"],
+        montserrat: ["Montserrat", "sans-serif"],
+      },
+      content: {
+        wishtext: "url('./assets/WishText.png')",
+        abstractwaves: "url('./assets/AbstractWaves.png')",
+        sparkles: "url('./assets/Sparkles.png')",
+        circles: "url('./assets/Circles.png')",
+        heroImage: "url('./assets/AfricaDrought.png')",
+      },
+    },
+    screens: {
+      xs: "480px",
+      sm: "768px",
+      md: "1060px",
+    },
+  },
+  plugins: [],
+};
