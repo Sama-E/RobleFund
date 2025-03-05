92aa295e5a8ca723198dac0928433ab7b94e60eb
Sama-E <112719147+Sama-E@users.noreply.github.com>
2025-03-05 13:17:44 -0500

    types update

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -23,11 +23,6 @@ module.exports = {
         montserrat: ["Montserrat", "sans-serif"],
       },
       content: {
-        // wishtext: "url('./assets/WishText.png')",
-        // abstractwaves: "url('./assets/AbstractWaves.png')",
-        // sparkles: "url('./assets/Sparkles.png')",
-        // circles: "url('./assets/Circles.png')",
-        // heroImage: "url('./assets/AfricaDrought.png')",
       },
     },
     screens: {
